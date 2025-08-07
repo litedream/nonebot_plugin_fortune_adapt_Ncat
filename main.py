879e10bf0b1429a7c9da54774cd3603c7946d4c9
xiaomeng <xiao_meng30@qq.com>
2025-08-07 22:55:00 +0800

    统一命令入口为messageHandler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import override
 from ncatbot.core import BaseMessage, GroupMessage, MessageChain
 from ncatbot.utils import get_log
 
+from module.Handler.message import messageHandler
 from . import nonebot_plugin_fortune
 from ncatbot.plugin.base_plugin import BasePlugin
 
@@ -16,9 +17,8 @@ class FortunePlugin(BasePlugin):
     async def on_load(self):
         await nonebot_plugin_fortune.fortune_check(self.data.get("config", {}))
         self.add_scheduled_task(interval="00:00", job_func=nonebot_plugin_fortune.cleanOutPics, name="cleanOutPics")
-        self.register_user_func(name="generalDivine", handler=userCommand, regex=r"^/(今日运势|抽签|运势)$")
 
-
+@messageHandler.on_message(r"^/(今日运势|抽签|运势)$")
 async def userCommand(msg: BaseMessage):
     if isinstance(msg, GroupMessage):
         resp = await nonebot_plugin_fortune.generalDivine(msg.raw_message, str(msg.group_id), str(msg.user_id))
